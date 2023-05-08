db33b4bcdbf0fd25f726da05255feb098856959e
Anshika Agarwal <anshika@Anshikas-MacBook-Pro.local>
2023-05-08 11:21:02 +0530

    Added JS code

--- album.js
+++ album.js
@@ -1,9 +1,66 @@
-const toggleButton = document.getElementById("toggle-button-id");
-const navLinks = document.getElementById("navbar-links-id");
+// const toggleButton = document.getElementById("toggle-button-id");
+// const navLinks = document.getElementById("navbar-links-id");
 
-toggleButton.addEventListener('click', function() {
-    navLinks.classList.toggle('active');
-})
+// toggleButton.addEventListener('click', function() {
+//     navLinks.classList.toggle('active');
+// })
 
- console.log(toggleButton);
+//  console.log(toggleButton);
+
+ const albums = document.getElementsByClassName('albums')[0];
+ const button = document.getElementById("addAlbum");
+
+ button.addEventListener('click', function() {
+
+    //e.preventDefault();
+
+    const imageURL = document.getElementById('image').value;
+    const description = document.getElementById('description').value;
+
+    const album = createAlbum(imageURL, description);
+    albums.appendChild(album);
+
+ })
+
+ function createAlbum(imageURL, description) {
+
+    const albumDiv = document.createElement('div'); // <div>
+    albumDiv.className = 'album';
+
+    const image = document.createElement('img');
+    image.setAttribute('src', imageURL);
+
+    const paragraph = document.createElement('p');
+    paragraph.textContent = description;
+
+    const albumFooter = document.createElement('div');
+    albumFooter.className = "albumFooter";
+
+    const buttonDiv = document.createElement('div');
+    const button1 = document.createElement('button');
+    const button2 = document.createElement('button');
+    button1.textContent = 'view';
+    button2.textContent = 'edit';
+
+      {/* <div>
+         <button>View</button>
+
+    </div> */}
+    buttonDiv.appendChild(button1);
+    buttonDiv.appendChild(button2);
+
+    const timeElement = document.createElement('span');
+    timeElement.textContent = '9 min';
+
+    albumFooter.appendChild(buttonDiv);
+    albumFooter.appendChild(timeElement);
+
+    albumDiv.appendChild(image);
+    albumDiv.appendChild(paragraph);
+    albumDiv.appendChild(albumFooter);
+
+    return albumDiv;
+
+ }
+
 
---
+++ jsconcepts.js
@@ -0,0 +1,63 @@
+
+// console.log(a);    // undefined
+
+// const a = 10;
+
+// console.log(a); // 10
+
+// function square (num) {
+//     return num * num;
+// }
+
+// var square1 = square(2);
+// var square2 = square(4);
+
+
+// JS Hoisting
+
+// getName();
+
+
+// var a = 89;
+
+// a = 78;
+
+// console.log(a);
+// function getName() {
+//     console.log("welcome to javascript");
+// }
+
+// if (true) {
+//     let a = 45;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+
+
+
+b();
+a();
+
+let x = 1;
+
+console.log(x);
+
+function a() {
+    x = 90;
+    console.log(x);
+}
+
+function b() {
+    x = 100;
+    console.log(x);
+}
+b();
+
+
+
+
+
+
+
