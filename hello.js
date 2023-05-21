3b2d70e88221b861ebc00da6ca59e8f53d117126
Anshika Agarwal <anshika@Anshikas-MacBook-Pro.local>
2023-05-21 10:32:56 +0530

    React basic concepts

---
+++ hello.js
@@ -0,0 +1,75 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+/**
+ * <div id="parent">
+ *      <div id="child1">
+ *          <h1> Hello World using React Elements <h1>
+ *          <h2> Hello World using React Elements 1 <h2>
+ *      </div>
+ * 
+ * <div id="child2">
+ *          <h1> Hello World using React Elements <h1>
+ *          <h2> Hello World using React Elements 1 <h2>
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement('div', {id: "parent"}, [
+    React.createElement('div', {id: "child1"}, [
+        React.createElement('h1', {}, "Hello World using React Elements"),
+        React.createElement('h2', {}, "Hello World using React Elements 1 ")
+    ]),
+
+    React.createElement('div', {id: "child2"}, [
+        React.createElement('h1', {}, "Hello World using React Elements"),
+        React.createElement('h2', {}, "Hello World using React Elements 1 ")
+    ]),
+]
+    );
+
+
+    //JSX --- HTML like syntax
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const header = (
+    <>
+         <div id="parent">
+        <div id="child">
+            <h1>Hiiii</h1>
+        </div>
+
+    </div>
+        <h1 id="heading"> Hello World </h1> 
+    </>
+   
+);
+
+console.log(header);
+
+// Babel -----  converts html like syntax to JS Object (React Element)
+
+root.render(header);
+
+
+
+//root.render(parent);
+// replace
+//root.render("anshika");
+
+
+// const header = React.createElement('h1', {id: "heading"}, 'Hello World');
+
+// // React Element ====> JS Object
+
+// console.log(header);
+
+// // ReactDOM is used for dom manipulation
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(header);
+
+// // render method converts your JS object into html (which browser understands)
+
+
